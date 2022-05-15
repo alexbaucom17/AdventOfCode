ef9d17f956cfc15afa99a7a47229ce667c404276
Alex <alexbaucom17@gmail.com>
2022-05-14 22:41:35 -0700

    Day 24 done. AOC2021 complete!

--- AdventofCode2021/day24.py
+++ AdventofCode2021/day24.py
@@ -2,106 +2,261 @@ import sys
 sys.path.append('.')
 import aoc
 import math
+from joblib import Parallel, delayed
+import time
 
 data_rows = aoc.get_input(24, sample=False, index=2).splitlines()
 
-class ALU:
+class ALUGen:
 
-    def __init__(self):
-        self.registers = [0,0,0,0]
-        self.register_map = {'w': 0, 'x': 1, 'y': 2, 'z': 3}
+    def __init__(self, gen_debug: bool = False):
+        self.gen_str = "import math\ndef gen_fn(input_arr: list[int]):\n"
+        self.gen_str += "\tw=0\n\tx=0\n\ty=0\n\tz=0\n"
 
-    def _get_register(self, reg):
-        return self.registers[self.register_map[reg]]
+        self.input_count = 0
+        self.gen_debug = gen_debug
 
-    def _get_value(self, val_or_reg):
-        if val_or_reg in self.register_map:
-            return self._get_register(val_or_reg)
-        else:
-            return int(val_or_reg)
+        self.gen_block_str = ""
 
-    def _set_register(self, reg, val):
-        self.registers[self.register_map[reg]] = val
+        if self.gen_debug:
+            self.gen_str += self._gen_line("print(input_arr)")
 
-    def input(self, register, val):
-        self._set_register(register, val)
+    def write(self):
+        if self.gen_debug:
+            self.gen_st += self._gen_line("print(f'Before return:       w={w} x={x} y={y} z={z}')")
+        self.gen_str += self._gen_line("return z")
+        self.gen_str += self.gen_block_str
+        self.gen_str += self._gen_block_end()
 
-    def add(self, reg_a, val_or_reg_b):
-        val = self._get_register(reg_a) + self._get_value(val_or_reg_b)
-        self._set_register(reg_a, val)
+        name = "AdventofCode2021/day24_gen.py"
+        with open(name, "w") as f:
+            f.write(self.gen_str)
+        print(f"Generated file at {name}")
 
-    def mul(self, reg_a, val_or_reg_b):
-        val = self._get_register(reg_a) * self._get_value(val_or_reg_b)
-        self._set_register(reg_a, val)
+    def _gen_line(self, line):
+        return f"\t{line}\n"
 
-    def div(self, reg_a, val_or_reg_b):
-        val = math.floor(self._get_register(reg_a) / self._get_value(val_or_reg_b))
-        self._set_register(reg_a, val)
-
-    def mod(self, reg_a, val_or_reg_b):
-        val = self._get_register(reg_a) % self._get_value(val_or_reg_b)
-        self._set_register(reg_a, val)
-
-    def eql(self, reg_a, val_or_reg_b):
-        val = self._get_register(reg_a) == self._get_value(val_or_reg_b)
-        self._set_register(reg_a, int(val))
-
-    def __str__(self):
-        s = ""
-        for r in ['w','x','y','z']:
-            s += f"{r}:{self._get_register(r)} "
-        return s
-
-
-def parse_and_execute(program, inputs):
-    alu = ALU()
-    input_count = 0
-    for line in program:
-        parts = line.split()
+    def gen_op(self, op: str):
+        parts = op.split()
         inst = parts[0]
         args = parts[1:]
-
         if inst == "inp":
-            if input_count >= len(inputs):
-                raise ValueError(f"Attempted to read from index {input_count} of input array which is only of length {len(inputs)}")
-            alu.input(args[0], inputs[input_count])
-            input_count += 1
-        elif inst == "add":
-            alu.add(args[0], args[1])
-        elif inst == "mul":
-            alu.mul(args[0], args[1])
-        elif inst == "div":
-            alu.div(args[0], args[1])
-        elif inst == "mod":
-            alu.mod(args[0], args[1])
-        elif inst == "eql":
-            alu.eql(args[0], args[1])
+            self.gen_str += self._gen_inp_main(args)
+            self.gen_block_str += self._gen_inp_block( args)
+            self.input_count += 1
         else:
-            raise ValueError(f"Unsupported instruction type {int}")
+            self.gen_str += self._gen_op(inst, args)
+            self.gen_block_str += self._gen_op(inst, args)
 
-        print(f"{line} - {str(alu)}")
+    def _gen_inp_main(self, args):
+        s = ""
+        if self.gen_debug:
+            s += self._gen_line("print(f'Before input {0} ({{input_arr[{1}]}}): w={{w}} x={{x}} y={{y}} z={{z}}')".format(self.input_count, self.input_count))
+        s += self._gen_line(f"{args[0]}=input_arr[{self.input_count}]")
+        return s
 
-    print(f"{inputs} - {str(alu)}")
-    return alu._get_register('z') == 0
+    def _gen_inp_block(self, args):
+        s = ""
+        if self.input_count > 0:
+            s += self._gen_block_end()
+        s += f"\ndef block{self.input_count}(in_val, in_z):\n"
+        s += "\tx=0\n\ty=0\n\tz=in_z\n"
+        s += self._gen_line(f"{args[0]}=in_val")
+        return s
+
+    def _gen_block_end(self):
+        s = ""
+        if self.gen_debug:
+            s += self._gen_line("print(f'Before return {0} ({{in_val}}): w={{w}} x={{x}} y={{y}} z={{z}}')".format(self.input_count, self.input_count))
+        s += self._gen_line(f"return z")
+        return s
+
+    def _gen_op(self, inst, args):           
+        if inst == "add":
+            return self._gen_line(f"{args[0]}={args[0]}+{args[1]}")
+        elif inst == "mul":
+            if args[1] == '0':
+                return self._gen_line(f"{args[0]}=0 # Was multiply by 0")
+            else:
+                return self._gen_line(f"{args[0]}={args[0]}*{args[1]}")
+        elif inst == "div":
+            if args[1] == '1':
+                return self._gen_line(f"# Skipped {args[0]} div by 1")
+            else:
+                return self._gen_line(f"{args[0]}=math.floor({args[0]}/{args[1]})")
+        elif inst == "mod":
+            return self._gen_line(f"{args[0]}={args[0]}%{args[1]}")
+        elif inst == "eql":
+            return self._gen_line(f"{args[0]}=int({args[0]}=={args[1]})")
+        else:
+            raise ValueError(f"Unsupported instruction type {inst}")
+        
+def number_gen(nums, max_count):
+    count = 0
+    yield nums
+    while count < max_count:
+        for i in range(13, -1, -1):
+            nums[i] -= 1
+            if nums[i] > 0:
+                break
+            else:
+                nums[i] = 9
+        count += 1
+        yield nums
+
+def join_num(num):
+    s = ""
+    for n in num:
+        s += str(n)
+    return s
+
+def split_num(num):
+    return [int(c) for c in str(num)]
+
+def run_gen():
+    gen = ALUGen(gen_debug=False)
+    for line in data_rows:
+        gen.gen_op(line)
+    gen.write()
+run_gen()
+from day24_gen import *
+
+def run_block(i, in_val, z):
+    if i == 0:
+        return block0(in_val, z)
+    elif i == 1:
+        return block1(in_val, z)
+    elif i == 2:
+        return block2(in_val, z)
+    elif i == 3:
+        return block3(in_val, z)
+    elif i == 4:
+        return block4(in_val, z)
+    elif i == 5:
+        return block5(in_val, z)
+    elif i == 6:
+        return block6(in_val, z)
+    elif i == 7:
+        return block7(in_val, z)
+    elif i == 8:
+        return block8(in_val, z)
+    elif i == 9:
+        return block9(in_val, z)
+    elif i == 10:
+        return block10(in_val, z)
+    elif i == 11:
+        return block11(in_val, z)
+    elif i == 12:
+        return block12(in_val, z)
+    elif i == 13:
+        return block13(in_val, z)
+
+def run_all_blocks(input_arr):
+    z = 0
+    for i in range(14):
+        z = run_block(i, input_arr[i], z)
+    return z
 
 def testing():
     n = 13579246899999
     nums = [int(c) for c in str(n)]
-    valid = parse_and_execute(data_rows, nums)
-    print(valid)
-testing()
+    for i in range(10):
+        nums[0] = i
+        gen_fn(nums)
+# testing()
+
+def test_blocks():
+    z = 24
+    z = run_block(12, 9, z)
+    print(z)
+    z = run_block(13, 9, z)
+    print(z)
+# test_blocks()
+
+def test_block_math():
+    n = 99999999999959
+    nums = split_num(n)
+    print(f"Blocks: {run_all_blocks(nums)}, Full: {gen_fn(nums)}")
+# test_block_math()
+
+def block_search_helper(block_num, in_z, valid_end_zs):
+    results = []
+    for in_val in [1,2,3,4,5,6,7,8,9]:
+        end_z = run_block(block_num, in_val, in_z)
+        # print(f"Block{block_num}(in_val={in_val}, in_z={in_z}) = {end_z}")
+        if end_z in valid_end_zs:
+            results.append((in_val, in_z, end_z))
+    return results
+
+def flatten_results(res):
+    out = []
+    for r in res:
+        if len(r) > 0:
+            out += r
+    return out
+
+def block_search_par(block_num, max_count, valid_end_zs, parallel):
+    res = parallel(delayed(block_search_helper)(block_num, in_z, valid_end_zs) for in_z in range(1, max_count))
+    return flatten_results(res)
+
+def block_search(block_num, max_count, valid_end_zs):
+    res = []
+    for in_z in range(1, max_count):
+        res.append(block_search_helper(block_num, in_z, valid_end_zs))
+    return flatten_results(res)
+
+def backwards_search(max_count):
+    seqs = {0: []}
+    with Parallel(n_jobs=16) as parallel:
+        for i in range(13, 0, -1):
+            count = max_count
+            if i > 11:
+                count = max_count / 10
+            results = block_search_par(i, max_count, seqs.keys(), parallel)
+            # results = block_search(i, max_count, seqs.keys())
+            new_seqs = {}
+            for r in results:
+                in_val, in_z, end_z = r
+                if in_z in new_seqs:
+                    s1 = [in_val] + seqs[end_z]
+                    s2 = new_seqs[in_z]
+                    # print(f'Checking {s1} vs {s2} for in_z {in_z}')
+                    if int(join_num(s1)) < int(join_num(s2)):
+                        new_seqs[in_z] = s1
+                else:
+                    new_seqs[in_z] = [in_val] + seqs[end_z]
+            seqs = new_seqs
+            print(f"Results for block{i}: Results - {len(results)}, Sequence map: {len(seqs)}")
+
+    last_results = block_search_helper(0, 0, seqs.keys())
+    all_nums = []
+    for r in last_results:
+        in_val, in_z, end_z = r
+        num = [in_val] + seqs[end_z]
+        n = int(join_num(num))
+        all_nums.append(n)
+    print(all_nums)
+
+    best_num = 9999999999999999999
+    for n in all_nums:
+        if n < best_num:
+            best_num = n
+    print(best_num)
+
 
 def part1():
-    start_num = 99999999999999
-    end_num = 99999999991991
-    for n in range(start_num, end_num, -1):
-        nums = [int(c) for c in str(n)]
-        if 0 in nums:
-            continue
-        valid = parse_and_execute(data_rows, nums)
-        if valid:
-            print(f"Found valid num: {n}")
-            break
+    max_count = 1000000
+    backwards_search(max_count)
+part1()
 
-# part1()
+
+# import cProfile
+# pr = cProfile.Profile()
+# pr.enable()
+# pr.run('part1()')
+# pr.disable()
+# import pstats
+# p = pstats.Stats(pr)
+# p.sort_stats(pstats.SortKey.TIME)
+# p.print_stats()
 
---
+++ AdventofCode2021/day24_gen.py
@@ -0,0 +1,595 @@
+import math
+def gen_fn(input_arr: list[int]):
+	w=0
+	x=0
+	y=0
+	z=0
+	w=input_arr[0]
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	# Skipped z div by 1
+	x=x+12
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+6
+	y=y*x
+	z=z+y
+	w=input_arr[1]
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	# Skipped z div by 1
+	x=x+10
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+6
+	y=y*x
+	z=z+y
+	w=input_arr[2]
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	# Skipped z div by 1
+	x=x+13
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+3
+	y=y*x
+	z=z+y
+	w=input_arr[3]
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	z=math.floor(z/26)
+	x=x+-11
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+11
+	y=y*x
+	z=z+y
+	w=input_arr[4]
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	# Skipped z div by 1
+	x=x+13
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+9
+	y=y*x
+	z=z+y
+	w=input_arr[5]
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	z=math.floor(z/26)
+	x=x+-1
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+3
+	y=y*x
+	z=z+y
+	w=input_arr[6]
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	# Skipped z div by 1
+	x=x+10
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+13
+	y=y*x
+	z=z+y
+	w=input_arr[7]
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	# Skipped z div by 1
+	x=x+11
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+6
+	y=y*x
+	z=z+y
+	w=input_arr[8]
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	z=math.floor(z/26)
+	x=x+0
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+14
+	y=y*x
+	z=z+y
+	w=input_arr[9]
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	# Skipped z div by 1
+	x=x+10
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+10
+	y=y*x
+	z=z+y
+	w=input_arr[10]
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	z=math.floor(z/26)
+	x=x+-5
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+12
+	y=y*x
+	z=z+y
+	w=input_arr[11]
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	z=math.floor(z/26)
+	x=x+-16
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+10
+	y=y*x
+	z=z+y
+	w=input_arr[12]
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	z=math.floor(z/26)
+	x=x+-7
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+11
+	y=y*x
+	z=z+y
+	w=input_arr[13]
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	z=math.floor(z/26)
+	x=x+-11
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+15
+	y=y*x
+	z=z+y
+	return z
+
+def block0(in_val, in_z):
+	x=0
+	y=0
+	z=in_z
+	w=in_val
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	# Skipped z div by 1
+	x=x+12
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+6
+	y=y*x
+	z=z+y
+	return z
+
+def block1(in_val, in_z):
+	x=0
+	y=0
+	z=in_z
+	w=in_val
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	# Skipped z div by 1
+	x=x+10
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+6
+	y=y*x
+	z=z+y
+	return z
+
+def block2(in_val, in_z):
+	x=0
+	y=0
+	z=in_z
+	w=in_val
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	# Skipped z div by 1
+	x=x+13
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+3
+	y=y*x
+	z=z+y
+	return z
+
+def block3(in_val, in_z):
+	x=0
+	y=0
+	z=in_z
+	w=in_val
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	z=math.floor(z/26)
+	x=x+-11
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+11
+	y=y*x
+	z=z+y
+	return z
+
+def block4(in_val, in_z):
+	x=0
+	y=0
+	z=in_z
+	w=in_val
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	# Skipped z div by 1
+	x=x+13
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+9
+	y=y*x
+	z=z+y
+	return z
+
+def block5(in_val, in_z):
+	x=0
+	y=0
+	z=in_z
+	w=in_val
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	z=math.floor(z/26)
+	x=x+-1
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+3
+	y=y*x
+	z=z+y
+	return z
+
+def block6(in_val, in_z):
+	x=0
+	y=0
+	z=in_z
+	w=in_val
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	# Skipped z div by 1
+	x=x+10
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+13
+	y=y*x
+	z=z+y
+	return z
+
+def block7(in_val, in_z):
+	x=0
+	y=0
+	z=in_z
+	w=in_val
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	# Skipped z div by 1
+	x=x+11
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+6
+	y=y*x
+	z=z+y
+	return z
+
+def block8(in_val, in_z):
+	x=0
+	y=0
+	z=in_z
+	w=in_val
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	z=math.floor(z/26)
+	x=x+0
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+14
+	y=y*x
+	z=z+y
+	return z
+
+def block9(in_val, in_z):
+	x=0
+	y=0
+	z=in_z
+	w=in_val
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	# Skipped z div by 1
+	x=x+10
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+10
+	y=y*x
+	z=z+y
+	return z
+
+def block10(in_val, in_z):
+	x=0
+	y=0
+	z=in_z
+	w=in_val
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	z=math.floor(z/26)
+	x=x+-5
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+12
+	y=y*x
+	z=z+y
+	return z
+
+def block11(in_val, in_z):
+	x=0
+	y=0
+	z=in_z
+	w=in_val
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	z=math.floor(z/26)
+	x=x+-16
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+10
+	y=y*x
+	z=z+y
+	return z
+
+def block12(in_val, in_z):
+	x=0
+	y=0
+	z=in_z
+	w=in_val
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	z=math.floor(z/26)
+	x=x+-7
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+11
+	y=y*x
+	z=z+y
+	return z
+
+def block13(in_val, in_z):
+	x=0
+	y=0
+	z=in_z
+	w=in_val
+	x=0 # Was multiply by 0
+	x=x+z
+	x=x%26
+	z=math.floor(z/26)
+	x=x+-11
+	x=int(x==w)
+	x=int(x==0)
+	y=0 # Was multiply by 0
+	y=y+25
+	y=y*x
+	y=y+1
+	z=z*y
+	y=0 # Was multiply by 0
+	y=y+w
+	y=y+15
+	y=y*x
+	z=z+y
+	return z
